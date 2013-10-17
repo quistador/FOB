5abaf1533591241eb4e3062750a4cf2d65ec86fc
summerquistador <brad@brad-apollo.(none)>
2013-10-16 20:10:57 -0700

    adding new files to flesh out further gameplay

---
+++ Scripts/Army.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Army 
+{
+	public Army(Profiler player)
+	{
+	}
+}
---
+++ Scripts/Profile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Profile
+{
+	private Army army;
+	
+	public Profile(string player)
+	{
+		
+	}
+}
